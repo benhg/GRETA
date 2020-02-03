3d0d63cea4f1078a6a6c82fb6d4e6e7d906940e7
Ben Glick <glick@glick.cloud>
2020-02-03 12:10:21 -0800

    Update all.py

--- all.py
+++ all.py
@@ -12,27 +12,29 @@ from STAR_index import star_index
 from STAR_align import star_align
 
 proteomefile = sys.argv[1]
-directory = '/home/users/ellenrichards/'
+directory = f'/home/users/ellenrichards/{sys.argv[2]}/'
 threshold = 1000
 
-generate_data(proteomefile)
+os.makedirs(directory)
 
-os.system("touch hopper.txt")
-os.system("touch final.txt")
-os.system("touch never.txt")
-os.system("touch maybehopper.txt")
-os.system("touch index_hopping_output.txt")
+generate_data(proteomefile, directory)
+
+os.system(f"touch {directory}/hopper.txt")
+os.system(f"touch {directory}/final.txt")
+os.system(f"touch {directory}/never.txt")
+os.system(f"touch {directory}/maybehopper.txt")
+os.system(f"touch {directory}/index_hopping_output.txt")
 
 #csv_filename = "index_hopping_output.csv"
 #csv_fh = open(csv_filename, "write")
 #csv_writer = csv.DictWriter(csv_fh, fieldnames=["filename", "uniquely", "multi", "totalReads", "uniquelyAGAINST", "multiAGAINST", "totalReadsAGAINST", "percratio"])
 #csv_row = {"filename": "filename ", "uniquely": "uniquely ", "multi": "multi ", "totalReads": "totalreads ", "uniquelyAGAINST": "uniquelyC ", "multiAGAINST": "multiC ", "totalReadsAGAINST": "totalreadsC ", "percratio": "percRatio "}
 #csv_writer.writerow(csv_row)
-os.system("echo 'filename  uniquely  multi  totalReads  uniquelyAGAINST  multiAGAINST  totalreadsAGAINST  percRatio'  >> index_hopping_output.txt")
+os.system(f"echo 'filename  uniquely  multi  totalReads  uniquelyAGAINST  multiAGAINST  totalreadsAGAINST  percRatio'  >> {directory}/index_hopping_output.txt")
 
-for filename in open("filenames.txt"):
+for filename in open(f"{directory}/filenames.txt"):
     os.chdir(directory)
-    mvalue = str(filename)
+    mvalue = str(f"{filename}")
     mvalue = mvalue.split("/n")[0]
     mvalue= mvalue.split('.fasta')[0]
 
@@ -41,7 +43,7 @@ for filename in open("filenames.txt"):
     svalue  = int(svalue)
     strsvalue = str(svalue)
 
-    os.makedirs(mvalue)
+    os.makedirs(f"{directory}/mvalue")
     genomeDir = f"{directory}{mvalue}/gd"
     os.makedirs(genomeDir)
 
@@ -75,7 +77,7 @@ for filename in open("filenames.txt"):
     totalreads = os.popen("awk 'FNR == 6{print $6}' " + strsvalue+ "Log.final.out").read()
 
     if uniquely + multi >= threshold:
-        os.system("echo " + filename + ">> final.txt") 
+        os.system("echo " + filename + f">> {directory}/final.txt") 
         output = 1
         uniquelyC = 'NO'
         multiC = 'NO'
@@ -91,7 +93,7 @@ for filename in open("filenames.txt"):
         counter = 15
 
     while output  == 0 and counter > 0:
-        os.system("echo" + filename + " >> maybehopper.txt")
+        os.system("echo" + filename + f" >> {directory}/maybehopper.txt")
         if counter == svalue: 
             counter +=1
         if counter == 20 or counter == 7: 
@@ -121,7 +123,7 @@ for filename in open("filenames.txt"):
         
         if (multiC + uniquelyC) > threshold and percRatio > 100:
             output = 1
-            os.system("echo " + filename + ">> hopper.txt")
+            os.system("echo " + filename + f" >> {directory}/hopper.txt")
         else:
             if counter == 17: 
                 counter = 19
@@ -134,8 +136,8 @@ for filename in open("filenames.txt"):
     if output == 1: #it needs to be added to a file
  #       csv_row = {"filename": str(filename), "uniquely": str(uniquely), "multi": str(multi), "totalReads": str(totalreads), "uniquelyAGAINST": str(uniquelyC), "multiAGAINST": str(multiC), "totalReadsAGAINST": str(totalreadsC), "percratio": str(percRatio)}
   #      csv_writer.writerow(csv_row)
-        os.system("echo " + str(filename) + "  " +  str(uniquely) + "  " + str(multi) + "  " + str(totalreads) + "  " +  str(uniquelyC) + "  " +   str(multiC) + "  " + str(totalreadsC) + "  " +  str(percRatio)   + "  >> ../index_hopping_output.txt")
+        os.system("echo " + str(filename) + "  " +  str(uniquely) + "  " + str(multi) + "  " + str(totalreads) + "  " +  str(uniquelyC) + "  " +   str(multiC) + "  " + str(totalreadsC) + "  " +  str(percRatio)   + f" >> {directory}/index_hopping_output.txt")
 
 
     if output == 0:
-        os.system("echo " + filename + ">> never.txt")
+        os.system("echo " + filename + f" >> {directory}/never.txt")
